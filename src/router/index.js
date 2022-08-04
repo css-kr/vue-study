339ebdc370332c34a0b474fbb092627428ef9834
이길환 <mz_pos@woowafriends.com>
2022-08-04 10:56:53 +0900

    0804 pagination

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
     component: () => import("@/views/user/UserIndex.vue"),
     children: [
       {
-        path: "list",
+        path: "list/:page?",
         name: "userList",
         component: () => import("@/views/user/UserList"),
       },
--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    total : {{ total }}<br />
+    page : {{ page }}
     <ul>
       <li v-for="item in userList" :key="item.id">
         <router-link :to="{ name: 'userDetail', params: { id: item.id } }"
@@ -8,6 +10,15 @@
         </router-link>
       </li>
     </ul>
+
+    <div>
+      <router-link
+        v-for="item in total_pages"
+        :key="item"
+        :to="{ name: 'userList', params: { page: item } }"
+        >{{ item }}</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -18,19 +29,44 @@ export default {
   data() {
     return {
       userList: null,
+      page: 1,
+      per_page: 2,
+      total: 0,
+      total_pages: 0,
     };
   },
+  watch: {
+    "$route.params.page"(value) {
+      console.log("---------", value);
+      this.page = value;
+      this.getUserList();
+    },
+  },
   mounted() {
+    this.page = this.$route.params.page;
     this.getUserList();
   },
   methods: {
     async getUserList() {
-      const { data } = await axios.get("https://reqres.in/api/users");
+      const { data } = await axios.get("https://reqres.in/api/users", {
+        params: {
+          page: this.page,
+          per_page: this.per_page,
+        },
+      });
 
       this.userList = data.data;
+      this.total = data.total;
+      this.total_pages = data.total_pages;
 
       console.log(data);
     },
   },
 };
 </script>
+
+<style lange="scss" scoped>
+.router-link-exact-active {
+  color: red;
+}
+</style>
