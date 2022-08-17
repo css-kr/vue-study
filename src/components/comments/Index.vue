d1d6ee2040aa56856970783c621a840e43450391
박수진 <sujinpark@mz.co.kr>
2022-08-17 12:44:07 +0900

    작업자 : 박수진 | 작업내용 : 블로그 2일차

---
+++ src/components/comments/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div
+      v-for="item in comments"
+      :key="item.id"
+      style="border: 1px solid green"
+    >
+      <div v-html="item.content.rendered"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "commentList",
+  data() {
+    return {
+      //id: 0,
+      comments: null,
+      comment_per_page: 100,
+    };
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 0,
+    },
+  },
+  mounted() {
+    this.getComment();
+  },
+  methods: {
+    async getComment() {
+      const { data } = await axios(
+        "https://theme.sunflower.kr/wp-json/wp/v2/comments",
+        {
+          params: {
+            post: this.id,
+            per_page: this.comment_per_page,
+          },
+        }
+      );
+      this.comments = data;
+      console.log("comment", data);
+    },
+  },
+};
+</script>
