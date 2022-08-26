e70fea2227dacc92a1358bc02a5839673abe5369
박수진 <sujinpark@mz.co.kr>
2022-08-26 13:54:57 +0900

    작업자 : 박수진 | 작업내용 : 블로그 7일차

---
+++ src/components/pagination/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <pre>{{ page }}</pre>
+
+    <router-link :to="prev" v-if="page.page > 1">prev</router-link>
+
+    <router-link
+      :to="{ name: 'blogList', query: { page: item } }"
+      v-for="item in page.total_page"
+      :key="item"
+      >{{ item }}</router-link
+    >
+
+    <router-link :to="next" v-if="page.page < page.total_page"
+      >next</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "listPagination",
+  props: {
+    page: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    prev() {
+      let prevPage = this.page.page - 1;
+      if (prevPage >= this.page.total_page) {
+        prevPage = this.page.total_page;
+      }
+      return { name: "blogList", query: { page: prevPage } };
+    },
+    next() {
+      let nextPage = this.page.page + 1;
+      if (nextPage >= this.page.total_page) {
+        nextPage = this.page.total_page;
+      }
+      return { name: "blogList", query: { page: nextPage } };
+    },
+  },
+};
+</script>
--- src/views/blog/List.vue
+++ src/views/blog/List.vue
@@ -12,48 +12,62 @@
       <hr />
     </div>
     <!--    <pre>{{ posts }}</pre>-->
+    <list-pagination :page="page"></list-pagination>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import tagList from "@/components/tags/tag";
+import ListPagination from "@/components/pagination/Index";
 
 export default {
   name: "blogList",
   components: {
     tagList: tagList,
+    ListPagination: ListPagination,
   },
   data() {
     return {
       //선언
       posts: null,
       tags: [],
+      page: {
+        per_page: 3,
+        page: 1,
+        total: 0,
+        total_page: 0,
+      },
     };
   },
   mounted() {
+    this.page.page = Number(this.$route.query.page) || 1;
     this.getPost();
   },
   watch: {
     "$route.query"(value) {
+      this.page.page = Number(this.$route.query.page) || 1;
       console.log("$route.query", value);
       this.getPost();
     },
   },
   methods: {
     async getPost() {
-      const { data } = await axios(
+      const { data, headers } = await axios(
         `${process.env.VUE_APP_URL}/wp-json/wp/v2/posts`,
         {
           params: {
-            per_page: 7,
+            page: this.page.page,
+            per_page: this.page.per_page,
             tags: this.$route.query.tag,
           },
         }
       );
+      this.page.total = Number(headers[`x-wp-total`]);
+      this.page.total_page = Number(headers[`x-wp-totalpages`]);
+      console.log(this.page);
       //통신 후 대입
       this.posts = data;
-      console.log(data);
     },
   },
 };
