d3c30482c752c3ca9c2c67889a000d7a73ef1e28
joo <joo@mz.co.kr>
2022-08-29 13:07:05 +0900

    0826

--- src/views/blog/List.vue
+++ src/views/blog/List.vue
@@ -18,48 +18,66 @@
 
       <tag-list :items="item.tags"></tag-list>
     </div>
+    <list-pagination :page="page"></list-pagination>
     <!--    <pre>{{ posts }}</pre>-->
   </div>
 </template>
 <script>
 import axios from "axios";
 import tagList from "@/components/tags/tag";
+import listPagination from "@/components/pagination/Index";
 
 export default {
   name: "blogList",
   components: {
     tagList: tagList,
+    listPagination: listPagination,
   },
   data() {
     return {
       //선언
       posts: null,
+      tags: [],
+      page: {
+        per_page: 3,
+        page: 1,
+        total: 0,
+        total_page: 0,
+      },
     };
   },
   mounted() {
+    this.page.page = Number(this.$route.query.page) || 1;
     this.getPost();
   },
 
   watch: {
     "$route.query"(value) {
+      this.page.page = Number(this.$route.query.page) || 1;
       console.log("$route.query", value);
       this.getPost();
     },
   },
   methods: {
     async getPost() {
-      const { data } = await axios(
-        "https://theme.sunflower.kr/wp-json/wp/v2/posts",
+      const { data, headers } = await axios(
+        `${process.env.VUE_APP_URL}/wp-json/wp/v2/posts`,
         {
           params: {
-            per_page: 7,
+            page: this.page.page,
+            per_page: this.page.per_page,
             tags: this.$route.query.tag,
           },
         }
       );
+
+      this.page.total = Number(headers["x-wp-total"]);
+      this.page.total_page = Number(headers["x-wp-totalpages"]);
+
+      // console.log("headers", this.page);
+
       //통신 후 대입
       this.posts = data;
-      // console.log(data);
     },
   },
 };
